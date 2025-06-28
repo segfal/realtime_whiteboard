8ab39c83a09384964720feaf0191bcb39c9a8dda
segfal <segfaultboi@gmail.com>
2025-06-28 12:54:52 -0400

    new implementation where we use webgl instead of creating a desktop app on opengl.

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>WHITEBOARD APP</h1>
+    </div>
+  )
+}
+
+export default App
